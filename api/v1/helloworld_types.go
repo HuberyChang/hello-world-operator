f23c1ee05240bb18f240e0880200bc557c869752
HuberyChang <1632425633@qq.com>
2023-05-13 00:54:27 +0800

    第一次提交逻辑代码

--- api/v1/helloworld_types.go
+++ api/v1/helloworld_types.go
@@ -44,8 +44,8 @@ type HelloWorldStatus struct {
 
 //+kubebuilder:object:root=true
 //+kubebuilder:subresource:status
-//+kubebuilder:printcolumn:JSONPath=".spec.size",name=Component,type=string
-//+kubebuilder:printcolumn:JSONPath=".spec.text",name=Component,type=string
+//+kubebuilder:printcolumn:JSONPath=".spec.size",name=Size,type=int32
+//+kubebuilder:printcolumn:JSONPath=".spec.text",name=Text,type=string
 
 // HelloWorld is the Schema for the helloworlds API
 type HelloWorld struct {
--- controllers/helloworld_controller.go
+++ controllers/helloworld_controller.go
@@ -18,6 +18,12 @@ package controllers
 
 import (
 	"context"
+	"fmt"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/api/meta"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/tools/record"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
@@ -25,12 +31,22 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/log"
 
 	opeartorv1 "github.com/HuberyChang/hello-world-operator/api/v1"
+	//apierrors "k8s.io/apimachinery/pkg/api/errors"
 )
+
+const helloworldFinalizer = "operator.example.com/finalizer"
+
+//typeAvailableHelloWorld 表示 Deployment 解耦的状态。
+const typeAvailableHelloWorld = "Available"
+
+//typeDegradedHelloWorld 表示当自定义资源被删除时必须发生的终结器操作的状态。
+const typeDegradedHelloWorld = "Degraded"
 
 // HelloWorldReconciler reconciles a HelloWorld object
 type HelloWorldReconciler struct {
 	client.Client
-	Scheme *runtime.Scheme
+	Scheme   *runtime.Scheme
+	Recorder record.EventRecorder
 }
 
 //+kubebuilder:rbac:groups=opeartor.example.com,resources=helloworlds,verbs=get;list;watch;create;update;patch;delete
@@ -47,11 +63,95 @@ type HelloWorldReconciler struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.12.1/pkg/reconcile
 func (r *HelloWorldReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	logger := log.FromContext(ctx)
 
 	// TODO(user): your logic here
+	helloworld := &opeartorv1.HelloWorld{}
+	err := r.Get(ctx, req.NamespacedName, helloworld)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			logger.Info("helloworld resource not found. Ignoring since object must be deleted")
+			return ctrl.Result{}, nil
+		}
+		logger.Error(err, "Failed to get helloworld")
+		return ctrl.Result{}, err
+	}
+
+	if helloworld.Status.Conditions == nil || len(helloworld.Status.Conditions) == 0 {
+		meta.SetStatusCondition(&helloworld.Status.Conditions, metav1.Condition{Type: typeAvailableHelloWorld, Status: metav1.ConditionUnknown,
+			Reason: "Reconciling", Message: "Starting reconciliation"})
+		if err = r.Status().Update(ctx, helloworld); err != nil {
+			logger.Error(err, "Failed to update HelloWorld status")
+			return ctrl.Result{}, err
+		}
+
+		if err := r.Get(ctx, req.NamespacedName, helloworld); err != nil {
+			logger.Error(err, "Failed to re-fetch helloworld")
+			return ctrl.Result{}, err
+		}
+	}
+
+	if !controllerutil.ContainsFinalizer(helloworld, helloworldFinalizer) {
+		logger.Info("Adding Finalizer for HelloWorld")
+		if ok := controllerutil.AddFinalizer(helloworld, helloworldFinalizer); !ok {
+			logger.Error(err, "Failed to add Finalizer into the custom resource")
+			return ctrl.Result{Requeue: true}, nil
+		}
+
+		if err = r.Update(ctx, helloworld); err != nil {
+			logger.Error(err, "Failed to update custom resource to add finalizer")
+			return ctrl.Result{}, err
+		}
+	}
+
+	isHelloWorldMarkedToBeDeleted := helloworld.GetDeletionTimestamp() != nil
+	if isHelloWorldMarkedToBeDeleted {
+		if controllerutil.ContainsFinalizer(helloworld, helloworldFinalizer) {
+			logger.Info("Performing finalizer operations for HelloWorld before delete CR")
+			meta.SetStatusCondition(&helloworld.Status.Conditions, metav1.Condition{Type: typeDegradedHelloWorld, Status: metav1.ConditionUnknown,
+				Reason: "Finalizing", Message: fmt.Sprintf("Performing finalizer operations for the custom resource: %s", helloworld.Name)})
+
+			if err := r.Status().Update(ctx, helloworld); err != nil {
+				logger.Error(err, "Failed to update HelloWorld status")
+				return ctrl.Result{}, err
+			}
+
+			r.doFinalizerOperationsForHelloWorld(helloworld)
+
+			if err := r.Get(ctx, req.NamespacedName, helloworld); err != nil {
+				logger.Error(err, "Failed to re-fetch helloworld")
+				return ctrl.Result{}, err
+			}
+
+			meta.SetStatusCondition(&helloworld.Status.Conditions, metav1.Condition{Type: typeDegradedHelloWorld, Status: metav1.ConditionTrue,
+				Reason: "Finalizing", Message: fmt.Sprintf("Finalizer operations for custom resource %s name were successfully accomplished",
+					helloworld.Name)})
+
+			if err := r.Status().Update(ctx, helloworld); err != nil {
+				logger.Error(err, "Failed to update HelloWorld status")
+				return ctrl.Result{}, err
+			}
+
+			logger.Info("Removing Finalizer for HelloWorld after successfully perform the operations")
+			if ok := controllerutil.RemoveFinalizer(helloworld, helloworldFinalizer); !ok {
+				logger.Error(err, "Failed to remove Finalizer for helloworld ")
+				return ctrl.Result{Requeue: true}, nil
+			}
+
+			if err := r.Update(ctx, helloworld); err != nil {
+				logger.Error(err, "Failed to remove finalizer for HelloWorld")
+				return ctrl.Result{}, err
+			}
+
+		}
+		return ctrl.Result{}, nil
+	}
 
 	return ctrl.Result{}, nil
+}
+
+func (r *HelloWorldReconciler) doFinalizerOperationsForHelloWorld(hd *opeartorv1.HelloWorld) {
+	r.Recorder.Event(hd, "Warning", "Deleting", fmt.Sprintf("Custom Resource %s is being deleted from the namespace %s", hd.Name, hd.Namespace))
 }
 
 // SetupWithManager sets up the controller with the Manager.
